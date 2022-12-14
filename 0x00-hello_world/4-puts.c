06ef05505ef61fbfc1e6385de004249b3fa7a428
sungu <jcliffland@gmail.com>
2022-12-14 04:09:17 -0800

    task 2-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -prints excatly "Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilinguaal puzzle")
+	return (0);
+}
