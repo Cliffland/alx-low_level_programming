e9b2b6a80b8f4a31d43af718b9d3b5c7808e5fa7
sungu <jcliffland@gmail.com>
2022-12-14 05:02:16 -0800

    5update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
-include <stdio.h>
+#include <stdio.h>
+
+
 
 /**
  *
- *  * main - prints with proper grammar, but the outcome is a piece of art,
+ *  * main - Entry point
  *
  *   *
  *
@@ -10,13 +12,12 @@ include <stdio.h>
  *
  *     */
 
-
-
 int main(void)
 
 {
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
 
 }
