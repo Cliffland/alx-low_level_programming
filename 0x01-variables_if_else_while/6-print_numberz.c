51ba245bbe7ef26a962edff6f824bf50635dd97b
sungu <jcliffland@gmail.com>
2022-12-23 08:19:54 -0800

    6-print_numberz.c update1

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,11 +7,13 @@
  */
 int main(void)
 {
-	int d;
+	int c = 0;
 
-	for (d = '0'; d <= '9'; d++)
-		putchar(d);
-		putchar('\n');
-
-	return (0);
+	while (c < 10)
+	{
+		putchar(48 + c);
+		c++;
+	}
+	putchar('\n');
+return(0);
 }
