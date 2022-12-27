74be02bc3c1d226245cc95ff0a247c5cb4526b61
sungu <jcliffland@gmail.com>
2022-12-27 10:11:05 -0800

    0-putchar.c update1

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * main - pritns _putchar as a message 
+ * main - pritns _putchar as a message
  *
  * return:always 0 (success)
  */
@@ -11,7 +11,7 @@ int main(void)
 	int count, sz;
 
 	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count ++)
+	for (count = 0; count < sz; count++)
 	{
 		_putchar(str[count]);
 	}
