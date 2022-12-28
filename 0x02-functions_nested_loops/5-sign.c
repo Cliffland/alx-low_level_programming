0c62ee5a0c75021d3dc0dac8a1473f4f3c445fbf
sungu <jcliffland@gmail.com>
2022-12-27 20:32:23 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: carrier variable
+ * description: prints the sign of a number
+ * return: 1 if n>0, if n==0, -1 if n<0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('_');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
+}
